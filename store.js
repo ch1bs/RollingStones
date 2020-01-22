e51a0b31f4c1bf99a8a2a7c2c526faf2a5c82a46
ch1bs <martin.slavov9309@gmail.com>
2020-01-22 17:12:13 +0200

    added updateCartTotal function

--- store.js
+++ store.js
@@ -1,27 +1,38 @@
-let removeCartItemButtons = document.getElementsByClassName('btn-danger');
-
-for (let i = 0; i < removeCartItemButtons.length; i++) {
-    let button = removeCartItemButtons[i];
-    button.addEventListener('click', function(event) {
-        let buttonClicked = event.target;
-        buttonClicked.parentElement.parentElement.remove();
-        updateCartTotall();
-    })
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
 }
 
-function updateCartTotall() {
+function ready() {
+    let removeCartItemButtons = document.getElementsByClassName('btn-danger');
+    console.log(removeCartItemButtons);
+
+    for (let i = 0; i < removeCartItemButtons.length; i++) {
+        let button = removeCartItemButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+}
+
+function removeCartItem(event) {
+    let buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+
+function updateCartTotal() {
     let cartItemContainer = document.getElementsByClassName('cart-items')[0];
     let cartRows = cartItemContainer.getElementsByClassName('cart-row');
     let total = 0;
-
     for (let i = 0; i < cartRows.length; i++) {
         let cartRow = cartRows[i];
         let priceElement = cartRow.getElementsByClassName('cart-price')[0];
         let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
         let price = parseFloat(priceElement.innerText.replace('$', ''));
         let quantity = quantityElement.value;
-        total += price * quantity;
+        total = total + (price * quantity);
     }
 
-    document.getElementsByClassName('cart-total-price')[0].innerText = total;
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName('cart-total-price')[0].innerText = `$${total}`;
 }
