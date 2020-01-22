5b418512c4cd86ef935c7d299095acd08b00e61f
ch1bs <martin.slavov9309@gmail.com>
2020-01-22 14:54:16 +0200

    added function for totalPrice

--- store.js
+++ store.js
@@ -9,6 +9,19 @@ for (let i = 0; i < removeCartItemButtons.length; i++) {
     })
 }
 
-function updateCartTotall(params) {
+function updateCartTotall() {
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    let total = 0;
 
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i];
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        let price = parseFloat(priceElement.innerText.replace('$', ''));
+        let quantity = quantityElement.value;
+        total += price * quantity;
+    }
+
+    document.getElementsByClassName('cart-total-price')[0].innerText = total;
 }
